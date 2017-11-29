f7406c009d4cc85600b2250764989cab428202d4
Abdullah Selek <abdullahselek@gmail.com>
2017-11-29 11:21:07 +0100

    update app function

--- reducepy/app.py
+++ reducepy/app.py
@@ -1,13 +1,24 @@
 from flask import Flask
+from flask import request
 from redis import Redis
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
-@app.route('/')
-def hello():
-    count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+@app.route('/', methods=['GET', 'POST'])
+def shorten():
+    error = None
+    if request.method == 'POST':
+        if 'url' in request.form:
+            url = request.form['url']
+            return 'URL : %s' % url
+        else:
+            error = 'Please post a url'
+    elif request.method == 'GET':
+        url = request.args.get('url', '')
+        return 'URL : %s' % url
+    else:
+        return 'Not supported method'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
