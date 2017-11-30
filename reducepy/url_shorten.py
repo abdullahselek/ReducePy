810294e8c377517f611e32028d370928ae91611e
Abdullah Selek <abdullahselek@gmail.com>
2017-11-30 16:32:36 +0100

    url shorten helper functions

--- reducepy/url_shorten.py
+++ reducepy/url_shorten.py
@@ -8,3 +8,17 @@ class UrlShorten(object):
     @staticmethod
     def md5(url):
         return hashlib.md5(url.encode('utf-8')).hexdigest()
+
+    @staticmethod
+    def byte_array(url):
+        byte_array = bytearray()
+        byte_array.extend(map(ord, url))
+        return byte_array
+
+    @staticmethod
+    def get_last_x_element(byte_array, x):
+        return byte_array[-x:]
+
+    @staticmethod
+    def string_from_bytes(byte_array):
+        return "".join(map(chr, byte_array))
--- tests/test_url_shorten.py
+++ tests/test_url_shorten.py
@@ -8,3 +8,17 @@ class UrlShorterTest(unittest.TestCase):
     def test_md5(self):
         md5_text = UrlShorten.md5('https://www.google.com')
         self.assertEqual(md5_text, '8ffdefbdec956b595d257f0aaeefd623')
+
+    def test_byte_array(self):
+        byte_array = UrlShorten.byte_array('https://www.google.com')
+        self.assertIsNotNone(byte_array)
+
+    def test_get_last_x_element(self):
+        byte_array = UrlShorten.byte_array('https://www.google.com')
+        last_four_element = UrlShorten.get_last_x_element(byte_array, 4)
+        self.assertEqual(len(last_four_element), 4)
+
+    def test_string_from_bytes(self):
+        bytes = [112, 52, 52]
+        string = UrlShorten.string_from_bytes(bytes)
+        self.assertEqual(string, 'p44')
