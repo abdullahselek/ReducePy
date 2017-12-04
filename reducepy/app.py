44011517138ad1df00b2ff8571e24a974700bd73
Abdullah Selek <abdullahselek@gmail.com>
2017-12-04 19:34:02 +0100

    create generic function to shorten url for app

--- reducepy/app.py
+++ reducepy/app.py
@@ -13,21 +13,22 @@ app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 store = Store(redis)
 
+def __create_shorten_url(url):
+    unique, short_url = UrlShorten.shorten_url(url)
+    store.keep(unique, url)
+    return short_url
+
 @app.route('/', methods=['GET', 'POST'])
 def shorten():
     if request.method == 'POST':
         if 'url' in request.form:
             url = request.form['url']
-            unique, short_url = UrlShorten.shorten_url(url)
-            store.keep(unique, url)
-            return short_url
+            return __create_shorten_url(url)
         else:
             return 'Please post a url', 403
     elif request.method == 'GET':
         url = request.args.get('url', '')
-        unique, short_url = UrlShorten.shorten_url(url)
-        store.keep(unique, url)
-        return short_url
+        return __create_shorten_url(url)
 
 @app.route('/<unique>')
 def forward(unique):
@@ -36,7 +37,6 @@ def forward(unique):
         return redirect(url)
     else:
         return 'No url not found', 200    
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
