a891326f1ac9ad75d56d090bbd08be00420e850c
Abdullah Selek <abdullahselek@gmail.com>
2022-04-03 20:26:32 +0100

    Bump version.

--- reducepy/__init__.py
+++ reducepy/__init__.py
@@ -8,11 +8,11 @@ __author__ = "Abdullah Selek"
 __email__ = "abdullahselek.os@gmail.com"
 __copyright__ = "Copyright (c) 2017 Abdullah Selek"
 __license__ = "MIT License"
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 __url__ = "https://github.com/abdullahselek/ReducePy"
 __download_url__ = "https://github.com/abdullahselek/ReducePy"
 __description__ = "URL shortener service is written with Python using Tornado and Redis"
 
 from .app import MainHandler
-from .url_shorten import UrlShorten
 from .store import Store
+from .url_shorten import UrlShorten
